e5fe619a430b95586ec97611c1915b2af9cbc536
dalip8797 <dalip8797@gmail.com>
2018-06-08 18:07:15 +0530

    added p2 file

---
+++ p2.java
@@ -0,0 +1,24 @@
+class leap
+{
+  public static void main(String args[])
+  {
+     int a=100;
+	 if(a%4==0)
+	 
+		 System .out .println("this year is leap year!!");
+		 else
+			 System .out .println("this is not a leap year !!");
+}
+}
+
+
+
+
+
+
+
+
+	
+	 
+    
+	 

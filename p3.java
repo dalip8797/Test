287eccccd4451992a0423744c3f0229a615a834d
dalip8797 <dalip8797@gmail.com>
2018-06-08 19:00:12 +0530

    added p3 file

---
+++ p3.java
@@ -0,0 +1,15 @@
+public class reverse
+{
+   public static void main (String args[])
+   {
+      int no=5432,revnum=0,tempnum,lastdigit;
+	    while(no!=0)
+		{ 
+		tempnum=no%10;
+		last digit=num%10;
+		revnum=revnum*10+last digit;
+num=tempnum;
+		}
+		System .out .println("reverse number is");
+	}
+}	

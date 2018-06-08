9545196f448d28c5a48a511ad758417d9ea71e2e
dalip8797 <dalip8797@gmail.com>
2018-06-08 18:20:28 +0530

    added p4 file

---
+++ p4.java
@@ -0,0 +1,63 @@
+
+
+
+
+class biggest
+{
+
+    public static void main(string  arg[])
+  {
+   int a=78;
+   int b=89;
+   int c=67;
+   if(a>=b&&a>=c)
+   System .out .println("a is biggest");
+   else if(b>=c&& b>=a)
+   System .out .println("b is biggest");
+   else if(c>=a&&c>=b)
+   System .out .println("c is biggest");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
